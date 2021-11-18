591f136eeab646614c290b65dc5cf52eeffd53db
luanws <luan.w.silveira@gmail.com>
2021-11-18 16:17:44 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QSplashScreen
+
+from src.ui.main import MainWindow
+from src.utils import styles
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    splash_screen = QSplashScreen(QPixmap('res/img/splash.png'))
+    splash_screen.show()
+
+    main_window = MainWindow()
+    main_window.showMaximized()
+
+    splash_screen.close()
+
+    app.setStyleSheet(styles.get_qss_stylesheet('src/styles/global.qss'))
+    app.exec()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/ui/main/window.py
@@ -0,0 +1,58 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(709, 578)
+        MainWindow.setStyleSheet("QPushButton {\n"
+"    padding: 4px;\n"
+"    background-color: transparent;\n"
+"    border-radius: 8px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #ccc;\n"
+"    color: white;\n"
+"}\n"
+"")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setContentsMargins(16, 8, 16, 16)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setObjectName("listWidget")
+        self.verticalLayout.addWidget(self.listWidget)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.actionSair = QtWidgets.QAction(MainWindow)
+        self.actionSair.setObjectName("actionSair")
+        self.action_about = QtWidgets.QAction(MainWindow)
+        self.action_about.setObjectName("action_about")
+        self.actionVerificar_se_h_atualiza_es = QtWidgets.QAction(MainWindow)
+        self.actionVerificar_se_h_atualiza_es.setObjectName("actionVerificar_se_h_atualiza_es")
+        self.action_quit = QtWidgets.QAction(MainWindow)
+        self.action_quit.setObjectName("action_quit")
+        self.action_settings = QtWidgets.QAction(MainWindow)
+        self.action_settings.setObjectName("action_settings")
+        self.action_export_history = QtWidgets.QAction(MainWindow)
+        self.action_export_history.setObjectName("action_export_history")
+        self.action_advanced_search = QtWidgets.QAction(MainWindow)
+        self.action_advanced_search.setObjectName("action_advanced_search")
+        self.action_install_version = QtWidgets.QAction(MainWindow)
+        self.action_install_version.setObjectName("action_install_version")
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Circuitos amplificadores"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.actionSair.setText(_translate("MainWindow", "Sair"))
+        self.action_about.setText(_translate("MainWindow", "Sobre"))
+        self.actionVerificar_se_h_atualiza_es.setText(_translate("MainWindow", "Verificar se há atualizações"))
+        self.action_quit.setText(_translate("MainWindow", "Sair"))
+        self.action_settings.setText(_translate("MainWindow", "Configurações"))
+        self.action_export_history.setText(_translate("MainWindow", "Exportar histórico"))
+        self.action_advanced_search.setText(_translate("MainWindow", "Pesquisa avançada"))
+        self.action_install_version.setText(_translate("MainWindow", "Instalar versão"))
