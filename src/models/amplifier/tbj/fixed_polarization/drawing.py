1c7437ea8fa3e2018799f2452ab9afd5cfa437ce
luanws <luan.w.silveira@gmail.com>
2022-02-07 13:19:52 -0300

    feat(models/tbj): fixed polarization draw

--- src/models/amplifier/tbj/fixed_polarization/drawing.py
+++ src/models/amplifier/tbj/fixed_polarization/drawing.py
@@ -5,51 +5,7 @@ from src.utils import numerize
 from .input import Input
 
 
-def draw_void():
-    drawing = Drawing()
-
-    drawing += (transistor := elm.transistors.BjtNpn().right())
-
-    drawing.push()
-    drawing += elm.Resistor().up().label('Rc')
-    drawing += elm.Line().left().length(drawing.unit/2)
-
-    drawing.push()
-    drawing += elm.Dot()
-    drawing += elm.SourceV().up().reverse().label('Vcc')
-    drawing += elm.Ground().left()
-
-    drawing.pop()
-    drawing += elm.Line().left().length(drawing.unit/2)
-    drawing += elm.Resistor().down().label('Rb')
-    drawing += elm.Line().down().length(0.24*drawing.unit)
-    drawing += elm.Dot()
-
-    drawing.push()
-    drawing += elm.Line().to(transistor.base)
-
-    drawing.pop()
-    drawing += elm.Line().left().length(drawing.unit/4)
-    drawing += elm.Capacitor().left().reverse().label('Ci')
-    drawing += elm.Line().left().length(drawing.unit/4)
-    drawing += elm.Dot().label('Vi')
-
-    drawing += elm.Line().at(transistor.emitter).down().length(3*drawing.unit/4)
-    drawing += elm.Ground()
-
-    drawing.pop()
-    drawing += elm.Dot()
-    drawing += elm.Capacitor().right().label('Co')
-    drawing += elm.Dot().label('Vo')
-
-    return drawing
-
-
-def draw(amplifier_input: Input):
-    Rc = numerize.format(amplifier_input.Rc, unit='Ω')
-    Rb = numerize.format(amplifier_input.Rb, unit='Ω')
-    Vcc = numerize.format(amplifier_input.Vcc, unit='V')
-
+def __draw(Rc: str, Rb: str, Vcc: str):
     drawing = Drawing()
 
     drawing += (transistor := elm.transistors.BjtNpn().right())
@@ -74,16 +30,44 @@ def draw(amplifier_input: Input):
 
     drawing.pop()
     drawing += elm.Line().left().length(drawing.unit/4)
-    drawing += elm.Capacitor().left().reverse().label('Ci')
-    drawing += elm.Line().left().length(drawing.unit/4)
+    drawing += elm.Capacitor().left().reverse().label('Ci').length(0.5*drawing.unit)
+    drawing += elm.Resistor().left().label('Rs')
     drawing += elm.Dot().label('Vi')
 
-    drawing += elm.Line().at(transistor.emitter).down().length(3*drawing.unit/4)
+    drawing += elm.Line().at(transistor.emitter).down().length(drawing.unit*0.53)
     drawing += elm.Ground()
 
     drawing.pop()
     drawing += elm.Dot()
     drawing += elm.Capacitor().right().label('Co')
+
+    drawing.push()
+    drawing += elm.Dot()
+    drawing += elm.Resistor().down().label('Rl')
+    drawing += elm.Ground()
+
+    drawing.pop()
+    drawing += elm.Line().right().length(drawing.unit/4)
     drawing += elm.Dot().label('Vo')
 
     return drawing
+
+
+def draw_void():
+    return __draw(
+        Rc='Rc',
+        Rb='Rb',
+        Vcc='Vcc'
+    )
+
+
+def draw(amplifier_input: Input):
+    Rc = numerize.format(amplifier_input.Rc, unit='Ω')
+    Rb = numerize.format(amplifier_input.Rb, unit='Ω')
+    Vcc = numerize.format(amplifier_input.Vcc, unit='V')
+
+    return __draw(
+        Rc=Rc,
+        Rb=Rb,
+        Vcc=Vcc
+    )
