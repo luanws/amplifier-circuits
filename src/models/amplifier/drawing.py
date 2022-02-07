cf6929a84474d2b988c0b6f7e80a9055bbcd8485
luanws <luan.w.silveira@gmail.com>
2022-02-07 16:39:59 -0300

    feat(model): desenho do modelo equivalente triangular

--- src/models/amplifier/drawing.py
+++ src/models/amplifier/drawing.py
@@ -5,30 +5,33 @@ from src.utils import numerize
 from .output import Output
 
 
-def draw_equivalent_model_void():
+def __draw_equivalent_model(Zi: str, Zo: str):
     drawing = Drawing()
     drawing += elm.Dot().label('Vi')
-    drawing += elm.Resistor().right().label('Zi')
+    drawing += elm.Line().right()
+    drawing += elm.Resistor().down().label(Zi)
+
+    drawing.push()
+    drawing += elm.Line().right().color('white').length(drawing.unit*0.5)
+    drawing += elm.Line().up().color('white').length(drawing.unit*0.25)
     drawing += elm.Ground()
-    drawing += elm.Dot()
-    drawing += elm.SourceControlledI().right()
-    drawing += elm.Resistor().right().label('Zo')
+    drawing += elm.SourceControlledI().up().length(drawing.unit*0.5)
+    drawing += elm.Line().right().length(drawing.unit*0.25)
+    drawing += elm.Line().down().length(drawing.unit*0.25)
+    drawing += elm.Resistor().right().label(Zo)
     drawing += elm.Dot().label('Vo')
 
+    drawing.pop()
+    drawing += elm.Ground()
+
     return drawing
+
+
+def draw_equivalent_model_void():
+    return __draw_equivalent_model('Zi', 'Zo')
 
 
 def draw_equivalent_model(output: Output):
     Zi = numerize.format(output.Zi, unit='Ω')
     Zo = numerize.format(output.Zo, unit='Ω')
-
-    drawing = Drawing()
-    drawing += elm.Dot().label('Vi')
-    drawing += elm.Resistor().right().label(Zi)
-    drawing += elm.Ground()
-    drawing += elm.Dot()
-    drawing += elm.SourceControlledI().right()
-    drawing += elm.Resistor().right().label(Zo)
-    drawing += elm.Dot().label('Vo')
-
-    return drawing
+    return __draw_equivalent_model(Zi, Zo)
