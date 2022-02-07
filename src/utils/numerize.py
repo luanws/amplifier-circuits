54b30b19ad92fd2e82036d072c24c364f1f3781b
luanws <luan.w.silveira@gmail.com>
2022-02-07 16:10:34 -0300

    Update numerize.py

--- src/utils/numerize.py
+++ src/utils/numerize.py
@@ -1,21 +1,36 @@
 import math
 
+positive_suffixes: list[list[str]] = [
+    [''],
+    ['k', 'K'],
+    ['M'],
+    ['G'],
+    ['T'],
+    ['P']
+]
+
+negative_suffixes: list[list[str]] = [
+    ['m'],
+    ['μ', 'u'],
+    ['n'],
+    ['p']
+]
+
 
 def format(
     number: float, *,
     precision: int = 2,
-    positive_suffixes: list[str] = ['', 'K', 'M', 'G', 'T', 'P'],
-    negative_suffixes: list[str] = ['m', 'u', 'n', 'p'],
     unit=''
 ) -> str:
-    suffixes = positive_suffixes + negative_suffixes[::-1]
+    suffixes_options = positive_suffixes + negative_suffixes[::-1]
     if number == 0:
         return '0'
     magnitude = int(math.floor(math.log(abs(number), 1e3)))
     if magnitude <= len(positive_suffixes) - 1 and magnitude >= -len(negative_suffixes):
         number_str = f'{number/1e3**magnitude:.{precision}f}'
         number_str = number_str.strip('0').strip('.')
-        number_str = f'{number_str}{suffixes[magnitude]}{unit}'
+        suffix = suffixes_options[magnitude][0]
+        number_str = f'{number_str}{suffix}{unit}'
     else:
         number_str = f'{number:.{precision}e}{unit}'
     return number_str.replace('.', ',')
@@ -23,19 +38,20 @@ def format(
 
 def revert(
     number_str: str,
-    positive_suffixes: list[str] = ['', 'K', 'M', 'G', 'T', 'P'],
-    negative_suffixes: list[str] = ['m', 'u', 'n', 'p'],
     unit: str = ''
 ) -> float:
-    suffixes = positive_suffixes + negative_suffixes[::-1]
+    suffixes_options = positive_suffixes + negative_suffixes[::-1]
     number_str = number_str.replace(',', '.')
     number_str = number_str.replace(' ', '')
     if len(unit) > 0 and number_str.endswith(unit):
         number_str = number_str[:-len(unit)]
-    for i, suffix in enumerate(suffixes):
-        if number_str.endswith(suffix):
-            sorted_sufixes = negative_suffixes[::-1] + positive_suffixes
-            magnitude = sorted_sufixes.index(suffix) - len(negative_suffixes)
-            if magnitude != 0:
-                return float(number_str[:-len(suffix)]) * 1e3**magnitude
+
+    sorted_sufixes_options = negative_suffixes[::-1] + positive_suffixes
+    for suffix_options in suffixes_options:
+        for suffix in suffix_options:
+            if number_str.endswith(suffix):
+                magnitude = sorted_sufixes_options.index(
+                    suffix_options) - len(negative_suffixes)
+                if magnitude != 0:
+                    return float(number_str[:-len(suffix)]) * 1e3**magnitude
     return float(number_str)
